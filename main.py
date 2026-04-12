b0914ed47ed7cd7d3b500b504b97cf4762431c6f
sjunin399-create <sjunin399@gmail.com>
2026-04-12 17:25:52 -0300

    update main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-site = "https://www.google.com"
-resposta = requests.get(site)
-
-print(f"Testando conexão com: {site}")
-print(f"Status da resposta: {resposta.status_code}") # 200 significa que está OK!
