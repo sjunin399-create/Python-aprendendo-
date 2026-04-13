62c6aeab8925e46b1030289a8026cf5e882a0c33
sjunin399-create <sjunin399@gmail.com>
2026-04-13 12:45:15 -0300

    update ex013.py

--- ex013.py
+++ ex013.py
@@ -1,3 +1,3 @@
 salario = float(input('Qual o salario do funcionario?R$: '))
-aumento = salario + (salario * 15/100)
+aumento = salario + (salario * 15 / 100)
 print(f'O salario de {salario:.2f} com 15% de aumento sera {aumento:.2f}')
