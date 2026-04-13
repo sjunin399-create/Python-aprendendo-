9046c51b48657bc94cbdf03730de098cc1e56051
sjunin399-create <sjunin399@gmail.com>
2026-04-13 12:44:35 -0300

    update ex012.py

--- ex012.py
+++ ex012.py
@@ -1,6 +1,8 @@
 preco = float(input('Qual é o preço do produto? R$'))
 novo = preco - (preco * 5 / 100)
-print(f'O produto que custava R${preco}, na promoção com desconto de 5% vai custar R${novo}')
-#valor = float(input('Digite o valor do produto para o desconto de 5% '))
-#resu = valor- (valor*5/100)
-#print(f'O valor total sera {resu:.3f}')
+print(
+    f'O produto que custava R${preco}, na promoção com desconto de 5% vai custar R${novo}'
+)
+# valor = float(input('Digite o valor do produto para o desconto de 5% '))
+# resu = valor- (valor*5/100)
+# print(f'O valor total sera {resu:.3f}')
