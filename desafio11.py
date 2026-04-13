a6cf9178108ff7c863d63e3b13143b230d48ce54
sjunin399-create <sjunin399@gmail.com>
2026-04-13 12:48:08 -0300

    update desafio11.py

--- desafio11.py
+++ desafio11.py
@@ -1,5 +1,5 @@
 largura = float(input('Qual a largura da parede'))
 altura = float(input('Qual a altura da parede'))
 tinta = largura * altura
-litros = tinta/2
+litros = tinta / 2
 print(f'Para pinta sua parede sera necessario {litros:.2f} litros de tinta')
