f42056a60dbd84e97614c9c112c9a528d1999fe7
sjunin399-create <sjunin399@gmail.com>
2026-04-13 16:03:37 -0300

    update ex018.py

--- ex018.py
+++ ex018.py
@@ -6,5 +6,5 @@ seno = math.sin(radianos)
 cosseno = math.cos(radianos)
 tangente = math.tan(radianos)
 print(
-    f'O seno, cosseno e tangente do ângulo {angulo} e. \nSeno:{seno}.\nCosseno:{cosseno}.\nTangente:{tangente}.'
+    f'O seno, cosseno e tangente do ângulo {angulo} e. \nSeno:{seno:.2f}.\nCosseno:{cosseno:.2f}.\nTangente:{tangente:.2f}.'
 )
