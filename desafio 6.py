a932e889a5d237c77f771291569ef0bfa294bed7
sjunin399-create <sjunin399@gmail.com>
2026-04-13 12:49:01 -0300

    update desafio 6.py

--- desafio 6.py
+++ desafio 6.py
@@ -1,6 +1,6 @@
 n1 = int(input('Digite um número:'))
-x2 = 2*n1
-x3 = 3*n1
-rq = n1**(1/2)
-#/n este comando irá quebrar a  linha.
+x2 = 2 * n1
+x3 = 3 * n1
+rq = n1 ** (1 / 2)
+# /n este comando irá quebrar a  linha.
 print(f'O dobro de {n1} é {x2}. \nO triplo é {x3}.\n E a raiz quadrada é {rq}')
