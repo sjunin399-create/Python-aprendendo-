0183d49ea8b55e0545aede35b06b6296735661b3
sjunin399-create <sjunin399@gmail.com>
2026-04-13 12:47:12 -0300

    update desafio8.py

--- desafio8.py
+++ desafio8.py
@@ -1,6 +1,8 @@
-medida  = int(input('Digite uma distância em metros: '))
-cm = medida*100
-mm = medida*1000
-hm = medida/100
-km = medida/1000
-print(f'A medida de {medida} metros corresponde a {cm}cm.\n {mm}mm.\n {hm}hm.\n {km}km.')
+medida = int(input('Digite uma distância em metros: '))
+cm = medida * 100
+mm = medida * 1000
+hm = medida / 100
+km = medida / 1000
+print(
+    f'A medida de {medida} metros corresponde a {cm}cm.\n {mm}mm.\n {hm}hm.\n {km}km.'
+)
