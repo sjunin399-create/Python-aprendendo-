4755733108f63dbc5bd9106b8ab024d273eec64d
sjunin399-create <sjunin399@gmail.com>
2026-04-13 15:28:53 -0300

    update ex016.py

--- ex016.py
+++ ex016.py
@@ -1,4 +1,5 @@
 import math
+
 num = float(input('Digite um numero flutuante: '))
 arredodamemto = math.floor(num)
-print (f'O arredodamemto do numero {num} e {arredodamemto}')
+print(f'O arredodamemto do numero {num} e {arredodamemto}')
