5d6cb080a33e842e18fbad8c5bc9818f60be2984
sjunin399-create <sjunin399@gmail.com>
2026-04-13 15:48:57 -0300

    update ex017.py

---
+++ ex017.py
@@ -0,0 +1,6 @@
+import math
+
+cateto1 = float(input('Digite o primeiro cateto: '))
+cateto2 = float(input('Digite o segundo cateto: '))
+raiz = math.sqrt((cateto1**2) + (cateto2**2))
+print(f'A valor da hipotenusa é {raiz}')
