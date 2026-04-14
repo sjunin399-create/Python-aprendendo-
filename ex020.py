4e28a6524133c6785c62b108f7bdde80d6119c60
sjunin399-create <sjunin399@gmail.com>
2026-04-14 17:08:24 -0300

    update ex020.py

--- ex020.py
+++ ex020.py
@@ -5,5 +5,5 @@ aluno2 = input('Dgite o nome do segundo: ')
 aluno3 = input('Dgite o nome do terceiro: ')
 aluno4 = input('Dgite o n]ome do quarto: ')
 lista = [aluno1, aluno2, aluno3, aluno4]
-embaralhar = math.suffle(lista)
+embaralhar = math.shuffle(lista)
 print(f'A ordem embaralhada foi {embaralhar}')
