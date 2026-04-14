85ac0cea685842af849262febc7325110aa43392
sjunin399-create <sjunin399@gmail.com>
2026-04-14 17:05:37 -0300

    update ex020.py

---
+++ ex020.py
@@ -0,0 +1,7 @@
+import math
+aluno1 = input('Dgite o nome do primeiro: ')
+aluno2 = input('Dgite o nome do segundo: ')
+aluno3 = input('Dgite o nome do terceiro: ')
+aluno4 = input('Dgite o nome do quarto: ')
+lista = [aluno1, aluno2, aluno3, aluno4]
+print(f'A ordem embaralhada foi {lista}')
