7f9ca2faddcc19e8e9f7a02aa31d7166c52601c8
sjunin399-create <sjunin399@gmail.com>
2026-05-09 20:05:32 -0300

    update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,2 +1,3 @@
 name = str(input('Qual seu nome completo: '))
-print(f' Seu nome com todas letras maiusculas é: {name.upper()}\n Seu nome com todas letras minusculas: {name.lower()}\n Seu ')
+pass1 
+print(f' Seu nome com todas letras maiusculas é: {name.upper()}\n Seu nome com todas letras minusculas: {name.lower()}\n Seu nome tem:{len(name)} ')
