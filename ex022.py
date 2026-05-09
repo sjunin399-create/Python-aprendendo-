f49ceeba452339f47dd60c1e3b20e7db144e9f15
sjunin399-create <sjunin399@gmail.com>
2026-05-09 20:24:41 -0300

    update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,5 +1,4 @@
 name = str(input('Qual seu nome completo: '))
-pass1 = name.replace(' ', '')
 print(
-    f' Seu nome com todas letras maiusculas é: {name.upper()}\n Seu nome com todas letras minusculas: {name.lower()}\n Seu nome tem:{len(pass1)} letras.\n No seu primeiro nome tem {len(name.split()[0])} letras.'
+    f' Seu nome com todas letras maiusculas é: {name.upper()}\n Seu nome com todas letras minusculas: {name.lower()}\n Seu nome tem: {len(name.replace(" ", ""))} letras.\n No seu primeiro nome tem {len(name.split()[0])} letras.'
 )
