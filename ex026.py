651a03ff13576245474ba1e21d7d2060e58d212a
sjunin399-create <sjunin399@gmail.com>
2026-05-10 20:10:38 -0300

    update ex026.py

--- ex026.py
+++ ex026.py
@@ -1,3 +1,5 @@
 phrase = str(input(' Digite uma frase: '))
 tradeA = phrase.replace('A', 'a')
-print(f']')
+print(
+    f'Nesta frase tem {tradeA.count("a")} A.\n O primeiro A aparece na posição {tradeA.find("a") + 1}. E o ultimo na posição {tradeA.rfind("a") + 1}.'
+)
