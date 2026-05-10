7cf64d341bd8fa01a6e1761d97614773709adf1f
sjunin399-create <sjunin399@gmail.com>
2026-05-10 13:30:35 -0300

    update ex023.py

---
+++ ex023.py
@@ -0,0 +1,4 @@
+number = input('Digite um numero com 4 digitos: ')
+print(
+    f' A unidade deste numero é: {number[0:1]}\n A dezena é: {number[1:2]}\n A centena: {number[2:3]}\n E a unidade de milhar: {number[3:4]}'
+)
