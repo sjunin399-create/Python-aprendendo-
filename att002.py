acdadaf0443576da9b8822d75f595363303562b7
sjunin399-create <sjunin399@gmail.com>
2026-05-12 18:15:51 -0300

    update att002.py

--- att002.py
+++ att002.py
@@ -4,3 +4,13 @@ pos_barra = url.find('/', pos_separador + 3)
 print(
     f' O protocolo e: {url[:pos_separador]}\n O dominio: {url[pos_separador + 3 : pos_barra]} \n Caminho: {url[pos_barra:]}'
 )
+# Verificamos se a barra foi encontrada
+if pos_barra == -1:
+    # Se não tem barra, o domínio começa após o :// e vai até o fim
+    dominio = url[pos_separador + 3 :]
+    caminho = 'Não possui caminho'
+else:
+    # Se tem barra, o domínio termina nela
+    dominio = url[pos_separador + 3 : pos_barra]
+    # E o caminho começa nela e vai até o fim
+    caminho = url[pos_barra:]
