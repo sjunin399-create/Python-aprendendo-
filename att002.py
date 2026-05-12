4ae3756733c17c0c0a880ab702b10aea89d73778
sjunin399-create <sjunin399@gmail.com>
2026-05-12 18:09:47 -0300

    update att002.py

--- att002.py
+++ att002.py
@@ -2,5 +2,5 @@ url = input('Digite um URL de um site: ')
 pos_separador = url.find('://')
 pos_barra = url.find('/', pos_separador + 3)
 print(
-    f' O protocolo e: {url[:5]}\n O dominio: {url[8:pos_barra]} \n Caminho: {url[pos_barra:]}'
+    f' O protocolo e: {url[:pos_separador]}\n O dominio: {url[8:pos_barra]} \n Caminho: {url[pos_barra:]}'
 )
